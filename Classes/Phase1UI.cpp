f63af9898eb0e8e334d53f512d8abd84c7d3ecff
Alhusseain Shalaby <80164892+alhusseain@users.noreply.github.com>
2023-12-11 23:24:54 +0200

    Create Phase1UI.cpp

---
+++ Classes/Phase1UI.cpp
@@ -0,0 +1,22 @@
+#include "Time_simulator.cpp";
+using namespace std;
+void begin_simulation()
+{
+	simulator simulate;
+	time_def begin_work("4:00");
+}
+/*
+int main()
+{
+	time_def last_time("1:24");
+	simulator simulate;
+	while (simulate.get_time_def() != last_time) {
+		simulate.plus_one();
+		cout << simulate.get_current_time() << endl;
+		Sleep(900);
+		system("cls");
+		
+
+	};
+}*/
+//NOT FINISHED........ELHUSSEAINS WORK
