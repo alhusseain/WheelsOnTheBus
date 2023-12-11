179e6c6e0c90317e96b8e8e7aeab320e91e6cefa
Ahmed Abdel Rahim <92340044+AhmedAbdelRahim1@users.noreply.github.com>
2023-12-11 22:23:11 +0200

    Update bus.cpp

--- Classes/bus.cpp
+++ Classes/bus.cpp
@@ -1,61 +1,39 @@
 #include <iostream>
-#include "LinkedList.h"
+#include "c++(priorityqueue).cpp"
+#include "station.h"
 using namespace std;
 
-// remove template when station and passenegrs are implemented
 template <typename T>
 class bus{
     private:
-        int id;
-        int capacity;
-        int numberOfJourneysDone;
-        int busyTime;
-        // replace t of the station to be a pointer to the station
-        T station;
-        int totalNumberOfPassengers;
-        // replace t of the currentPassengers to be a pointer to the passengers
-        LinkedList<T> currentPassengers;
-        int checkUpDuration;
-        bool isBusy;
-    public:
-        bus(int id, int capacity, int numberOfJourneys, int checkUpDuration){
-            this->id = id;
-            this->capacity = capacity;
-            this->numberOfJourneysDone = numberOfJourneys;
-            this->checkUpDuration = checkUpDuration;
-        }
-        int getId(){
-            return id;
-        }
-        int getCapacity(){
-            return capacity;
-        }
-        int getNumberOfJourneys(){
-            return numberOfJourneysDone;
-        }
+            int capacity;
+            int numberOfJourneysDoneBeforeCheckUp;
+            int checkUpDuration;
+            Stations<T> station; 
+            int NumberOfPassengers;
+            LinkedList<passengers> currentPassengers;
+            public:
+                bus(int capacity, int numberOfJourneysDoneBeforeCheckUp, int checkUpDuration){
+                    this->capacity = capacity;
+                    this->numberOfJourneysDoneBeforeCheckUp = numberOfJourneysDoneBeforeCheckUp;
+                    this->checkUpDuration = checkUpDuration;
+                }
+                int getCapacity(){
+                    return capacity;
+                }
+            int getNumberOfJourneys(){
+                return numberOfJourneysDoneBeforeCheckUp;
+            }
         int getCheckUpDuration(){
             return checkUpDuration;
-        }
-        void setId(int id){
-            this->id = id;
         }
         void setCapacity(int capacity){
             this->capacity = capacity;
         }
-        void setNumberOfJourneys(int numberOfJourneys){
-            this->numberOfJourneys = numberOfJourneys;
-        }
-        void setCheckUpDuration(int checkUpDuration){
-            this->checkUpDuration = checkUpDuration;
-        }
-        void addPassenger(int passengerId){
-            currentPassengers.Insert(passengerId);
-        }
-        void removePassenger(int passengerId){
-            currentPassengers.DeleteNode(passengerId);
-        }
+
         void printPassengers(){
-            currentPassengers.PrintList();
+            Node<passengers>* head = currentPassengers.gethead();
+            currentPassengers.displaypassengers(head);
         }
         int getNumberOfPassengers(){
             return currentPassengers.getLength();
@@ -66,23 +44,15 @@ class bus{
         bool isEmpty(){
             return currentPassengers.getLength() == 0;
         }
-        void setBusy(bool isBusy){
-            this->isBusy = isBusy;
-        }
-        bool getBusyStatus(){
-            return isBusy;
-        }
         ~bus(){
-            currentPassengers.DeleteAll();
+            currentPassengers.clear();
         }
 };
 
 template <typename T>
 class Wbus : public bus<T> {
-    private:
-        bus<T> bus; 
     public:
-        Wbus(int id, int capacity, int numberOfJourneys, int checkUpDuration) : bus<T>(id, capacity, numberOfJourneys, checkUpDuration){
+        Wbus(int capacity, int numberOfJourneysDoneBeforeCheckUp, int checkUpDuration) : bus<T>(capacity, numberOfJourneysDoneBeforeCheckUp, checkUpDuration){
         }
         ~Wbus(){
             ~Wbus();
@@ -93,30 +63,21 @@ class Wbus : public bus<T> {
 template <typename T>
 class Mbus: public bus<T>{
     public:
-        Mbus(int id, int capacity, int numberOfJourneys, int checkUpDuration) : bus<T>(id, capacity, numberOfJourneys, checkUpDuration){
+        Mbus(int capacity, int numberOfJourneysDoneBeforeCheckUp, int checkUpDuration) : bus<T>(capacity, numberOfJourneysDoneBeforeCheckUp, checkUpDuration){
         }
         ~Mbus(){
             ~Mbus();
         }
 };
 
-
 int main(){
-    bus<int> b(1, 10, 5, 10);
-    b.addPassenger(1);
-    b.addPassenger(2);
-    b.addPassenger(3);
-    b.addPassenger(4);
-    b.addPassenger(5);
-    b.printPassengers();
-    b.removePassenger(1);
-    b.removePassenger(2);
-    b.removePassenger(3);
-    b.removePassenger(4);
-    b.removePassenger(5);
-    b.printPassengers();
-    cout << b.getNumberOfPassengers() << endl;
-    cout << b.isFull() << endl;
-    cout << b.isEmpty() << endl;
+    bus<passengers> bus1(10, 5, 2);
+    bus1.printPassengers();
+    cout << bus1.getCapacity() << endl;
+    cout << bus1.getNumberOfJourneys() << endl;
+    cout << bus1.getCheckUpDuration() << endl;
+    cout << bus1.getNumberOfPassengers() << endl;
+    cout << bus1.isFull() << endl;
+    cout << bus1.isEmpty() << endl;
     return 0;
 }
