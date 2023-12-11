73a1f463e8f67152cc13e51e996d1d17978c4f05
Alhusseain Shalaby <80164892+alhusseain@users.noreply.github.com>
2023-12-11 23:21:46 +0200

    Update Filehandling_elhusseain.cpp

--- Classes/Filehandling_elhusseain.cpp
+++ Classes/Filehandling_elhusseain.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include"passengers.h"
+#include"station.h"
+#include"Events.cpp"
+#include"priorityqueue.h"
 using namespace std;
-class f_handler
+class company
 {
 	private:
 		int S; 		// number of stations
@@ -17,11 +21,22 @@ class f_handler
 		int maxW;		//maximum waiting time
 		int on_off_time;//get on/off time in seconds
 		int E; 			//number of events
+		/*char event_type;
+		string passenger_type;
+		string string_time;
+		time_def event_time;
+		int id;
+		int start_station, end_station;
+		string special_description;*/
+		//--------------------------------------------------------------------
+		Queue<Arrival_event> Arrival_events_list;
+		Queue<Leave_event> Leave_events_list;
+		char* event_types;
+
 	public:
-		f_handler()
+		company()
 		{
 			ifstream randomizer("random_file.txt");
-			string read;
 			randomizer>>S>>ST;
 			randomizer>>WBus_count>>MBus_count;
 			randomizer>>WBus_capacity>>MBus_capacity;
@@ -29,17 +44,47 @@ class f_handler
 			randomizer>>maxW>>on_off_time;
 			randomizer>>E;
 			
+	/*		event_types = new char[E];
+			for (int i = 0; i < E; i++)
+			{
+				randomizer >> event_type >> passenger_type >> string_time >> id;
+				if (event_type == 'A') {
+					randomizer >> start_station >> end_station;
+					if (passenger_type == "SP") randomizer >> special_description;
+				}
+				else randomizer >> start_station;
+				if (event_type == 'A')
+				{
+					Arrival_event new_event (start_station, end_station, passenger_type, special_description, string_time, id);
+					Arrival_events_list.enqueue(new_event, 0);
+				}
+				else {
+					Leave_event new_event(end_station,string_time, id);
+					Leave_events_list.enqueue(new_event, 0);
+				}
+				event_types[i] = event_type;
+				
+			}
+			
 		}
-		/* void display()
+		 void display()
 		{
 			cout<<S<<" "<<ST<<endl;
 			cout<<WBus_count<<" "<<MBus_count<<endl;
 			cout<<WBus_capacity<<" "<<MBus_capacity<<endl;
 			cout<<J<<" "<<C_WBus<<" "<<C_MBus<<endl;
 			cout<<maxW<<" "<<on_off_time<<endl;
-			cout<<E;
+			cout<<E<<endl<<endl;
+			for (int i; i < E; i++)
+			{
+				if (event_types[i] == 'A')
+				{
+					Arrival_events_list.dequeue(new_event)
+				}
+			}
 		}
-		*/
+		 */
+			
 		int num_of_stations() { return S;}
 		int time_between_stations(){return ST;}
 		int W_count(){return WBus_count;}
@@ -47,9 +92,16 @@ class f_handler
 		int W_capacity(){return WBus_capacity;}
 		int M_capacity(){return MBus_capacity;}
 		int num_before_checkup(){return J;}
-		int W_checkup_duration(){return C_Wbus;}
-		int M_checkup_duration(){return C_Mbus;}
+		int W_checkup_duration(){return C_WBus;}
+		int M_checkup_duration(){return C_MBus;}
 		int maximum_waiting_time(){return maxW;}
 		int get_on_off_time(){return on_off_time;}
+		void events_set_up()
+		{
+
+		}
+
 		
 };
+
+
