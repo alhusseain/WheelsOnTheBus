beebd262c585231c091de68585883dd613429fb9
Alhusseain Shalaby <80164892+alhusseain@users.noreply.github.com>
2023-12-11 02:24:11 +0200

    Upload time.cpp file

---
+++ Classes/time.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include<cstring>
+using namespace std;
+
+class time_def
+{
+private:
+	string same_time;
+	int hours,minutes;
+	int count_to_delimiter;
+	int total_minutes;
+public:
+	time_def(string time_string)
+	{
+		same_time = time_string;
+		count_to_delimiter = 0;
+		while (1)
+		{
+			if (time_string[count_to_delimiter] == ':') break;
+			else count_to_delimiter++;
+		}
+		if (count_to_delimiter == 1)
+		{
+			hours = stoi(time_string.substr(0,1));
+			minutes = stoi(time_string.substr(2, time_string.size() - 2));
+		}
+		else {
+			hours = stoi(time_string.substr(0, 2));
+			minutes = stoi(time_string.substr(3, time_string.size() - 3));
+		}
+	}
+	string get_time()
+	{
+		return same_time;
+	}
+	int convert_all_to_min()
+	{
+		total_minutes = hours * 60 + minutes;
+		return total_minutes;
+	}
+	string return_to_format_from_minutes(int minutes)
+	{
+		string to_return_hours = to_string(minutes / 60);
+		string to_return_minutes = to_string(minutes % 60);
+		return to_return_hours + ":" + to_return_minutes;
+		
+		
+
+	}
+
+	string operator+(time_def& x)
+	{
+		int current_class_minutes = convert_all_to_min();
+		int guest_class_minutes = x.convert_all_to_min();
+		int sum_minutes = current_class_minutes + guest_class_minutes;
+		return return_to_format_from_minutes(sum_minutes);
+	}
+
+
+	string operator-(time_def& x)
+	{
+		int current_class_minutes = convert_all_to_min();
+		int guest_class_minutes = x.convert_all_to_min();
+		int diff_minutes = abs(current_class_minutes - guest_class_minutes);
+		return return_to_format_from_minutes(diff_minutes);
+	}
+
+
+	/*
+	void display()
+	{
+		cout << "hours: " << hours << endl;
+		cout << "minutes: " << minutes;
+	}*/
+};
+/*
+int main()
+{
+	time_def inter("4:3"),inter2("5:57");
+	cout << inter + inter2;
+}*/
+
